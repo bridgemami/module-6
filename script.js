59d1660926a219d9776dbbe9ff356ea044a2f1b2
Michael <80859948+bridgemami@users.noreply.github.com>
2021-09-27 04:49:26 +0000

    added notes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
      You will need to replace the contents of this JavaScript file with your own work, 
      and create any other files, if any, required for the assignment.
      When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+//array for the quiz
 const myQuiz = [
   {
     number: "Question 1",
@@ -32,40 +33,52 @@ const myQuiz = [
     progress: 100
   }
 ];
+//turn tracker
 let turn= 0;
-
+// show first question after the page has fully loaded
 findQuestion();
-
+// function that can show the current question and its possible answers
 function findQuestion() {
+  // set the counter
   $('#move').empty(turn + 1);
+  //set progress bar
   $( "#progressbar" ).progressbar({
   value: myQuiz[turn].progress,
   max: 100
 });
+//show question number
   $('#number').text(myQuiz[turn].number);
+   // show question text
   $('#question').text(myQuiz[turn].question);
+  // render buttons for answers
   $('#answer').empty();
 
 for ( let mc in myQuiz[turn].answer) {
+  // 1. Create the button and set text to answer text
   var button = $("<span>");
   button.text(myQuiz[turn].answer[mc]);
+ // 2. Store a custom html attribute data value in the span
   button.data('choice', mc);
-
+// 3. Append button element into html document model
   $('#answer').append( button );
-
+// 4. Add event handler
   button.draggable(); 
 }
+ //make the #question droppable to receive the dragged answer
  $('#question').droppable(
     {
       drop: function(event, ui) {
         console.log( ui.draggable.data('choice') );
         var userchoice = ui.draggable.data('choice');
+        // did the user pick the correct choice?
         checkAnswer(userchoice);
       }
     }
   );
 }
 function checkAnswer(choice) {
+   // handle event when user clicks an answer: right or wrong?
+    // decide which answer is correct
 if (choice == myQuiz[turn].correct ) {
   $('#wrong').empty();
   //$('#right').html("<strong>Correct</strong>");
@@ -81,6 +94,7 @@ if (choice == myQuiz[turn].correct ) {
   } );
   nextQuestion();
 }
+//wrong answer
 else{
   
   //$('#wrong').html("<strong>Incorrect</strong>");
@@ -97,7 +111,7 @@ else{
   $('#right').empty();
 }
 }
-
+// show another if one is available
 function nextQuestion() {
   turn++;
   if( turn < myQuiz.length) {
